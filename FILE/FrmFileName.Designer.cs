7d4bda69ef3fff5355810ac89c006a487dd44a6f
CAIRO <CAIRO@DESKTOP-G85462B>
2025-10-18 01:14:44 +0800

    UI WITH REGISTRATION

--- FILE/FrmFileName.Designer.cs
+++ FILE/FrmFileName.Designer.cs
@@ -35,8 +35,8 @@
             // 
             // btnOkay
             // 
-            this.btnOkay.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.btnOkay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOkay.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnOkay.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Bold);
             this.btnOkay.Location = new System.Drawing.Point(58, 265);
             this.btnOkay.Name = "btnOkay";
             this.btnOkay.Size = new System.Drawing.Size(364, 49);
@@ -56,11 +56,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(54, 137);
             this.label1.Name = "label1";
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.Size = new System.Drawing.Size(99, 24);
             this.label1.TabIndex = 3;
             this.label1.Text = "Enter File Name";
             // 
@@ -68,6 +68,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(464, 450);
             this.Controls.Add(this.btnOkay);
             this.Controls.Add(this.txtFileName);
