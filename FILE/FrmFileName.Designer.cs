4039aed5c9efaf33e2593e8d5b2bf2486f970143
CAIRO <CAIRO@DESKTOP-G85462B>
2025-10-18 00:58:27 +0800

    UI with functions

---
+++ FILE/FrmFileName.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace FILE
+{
+    partial class FrmFileName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOkay = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOkay
+            // 
+            this.btnOkay.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnOkay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOkay.Location = new System.Drawing.Point(58, 265);
+            this.btnOkay.Name = "btnOkay";
+            this.btnOkay.Size = new System.Drawing.Size(364, 49);
+            this.btnOkay.TabIndex = 5;
+            this.btnOkay.Text = "Okay";
+            this.btnOkay.UseVisualStyleBackColor = false;
+            this.btnOkay.Click += new System.EventHandler(this.btnOkay_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(58, 182);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(364, 54);
+            this.txtFileName.TabIndex = 4;
+            this.txtFileName.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 137);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter File Name";
+            // 
+            // FrmFileName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 450);
+            this.Controls.Add(this.btnOkay);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmFileName";
+            this.Text = "FrmFileName";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOkay;
+        private System.Windows.Forms.RichTextBox txtFileName;
+        private System.Windows.Forms.Label label1;
+    }
+}
